a1505e5bcda66683ca9681d9cdfc35b17bf436b7
windihi <pupatsepilov@gmail.com>
2025-10-19 12:04:45 +0300

    after lesson

--- client.py
+++ client.py
@@ -1,16 +1,49 @@
 import socket
 import pygame as pg
 import math
+import tkinter as tk
+from tkinter import ttk
+import tkinter.messagebox
 
 WIDTH = 800
 HEIGHT = 600
 CENTER = (WIDTH//2,HEIGHT//2)
+NAME = None
 radius = 50
 ovec = (0,0)
+
+def check_and_login():
+    global NAME,COLOR
+    NAME = entry.get()
+    COLOR = combb.get()
+    if NAME != "" and COLOR != "":
+        root.destroy()
+    else:
+        tkinter.messagebox.showerror("Ошибка","Выберите имя и Цвет")
+
+root = tk.Tk()
+root.title("Вход")
+root.geometry("400x400")
+root["bg"] = "grey"
+
+COLORS = ['Maroon', 'DarkRed', 'FireBrick', 'Red', 'Salmon', 'Tomato', 'Coral', 'OrangeRed', 'Chocolate', 'SandyBrown', 'DarkOrange', 'Orange', 'DarkGoldenrod', 'Goldenrod', 'Gold', 'Olive', 'Yellow', 'YellowGreen', 'GreenYellow', 'Chartreuse', 'LawnGreen', 'Green', 'Lime', 'Lime Green', 'SpringGreen', 'MediumSpringGreen', 'Turquoise', 'LightSeaGreen', 'MediumTurquoise', 'Teal', 'DarkCyan', 'Aqua', 'Cyan', 'Dark Turquoise', 'DeepSkyBlue', 'DodgerBlue', 'RoyalBlue', 'Navy', 'DarkBlue', 'MediumBlue.']
+COLOR = tk.StringVar(value=COLORS[0])
+
+entry = ttk.Entry(root)
+entry.pack()
+combb = ttk.Combobox(root,values=COLORS,textvariable=COLOR)
+combb.pack()
+cbtn = ttk.Button(root,text="Вход",command=check_and_login)
+cbtn.pack()
+
+root.mainloop()
+
 
 mainsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 mainsocket.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
 mainsocket.connect(("localhost",12424))
+
+mainsocket.send(f"{NAME},{COLOR}".encode())
 
 pg.init()
 screen = pg.display.set_mode((WIDTH,HEIGHT))
@@ -31,12 +64,10 @@ while run:
             if vecc != ovec:
                 ovec = vecc
                 mainsocket.send(f"<{vecc[0]},{vecc[1]}>".encode())
-                print(f"<{vecc[0]},{vecc[1]}>")
 
     data = mainsocket.recv(1024).decode()
-    print(data)
     screen.fill("grey")
-    pg.draw.circle(screen,(255,0,0),CENTER,radius)
+    pg.draw.circle(screen,COLOR,CENTER,radius)
     pg.draw.line(screen,(0,0,0),CENTER,coords)
     pg.display.update()
 pg.quit()
--- internet_connection_21.09.25.py
+++ internet_connection_21.09.25.py
@@ -25,17 +25,20 @@ class Player(Base):
     x = Column(Integer,default=500)
     y = Column(Integer,default=500)
     size = Column(Integer,default=50)
+    color = Column(String(250), default="red")
     errors = Column(Integer,default=0)
     abs_speed = Column(Integer,default=1)
     speed_x = Column(Integer,default=0)
     speed_y = Column(Integer,default=0)
+    h_vision = Column(Integer,default=600)
+    w_vision = Column(Integer,default=800)
 
     def __init__(self,name,address):
         self.name = name
         self.address = address
 
 class LocalPlayer():
-    def __init__(self,id,name,socket,address):
+    def __init__(self,id,name,socket,address,color):
         self.id = id
         self.db: Player = s.get(Player, self.id)
         self.socket = socket
@@ -44,10 +47,42 @@ class LocalPlayer():
         self.x = 500
         self.y = 500
         self.size = 50
+        self.color = color
         self.errors = 0
         self.abs_speed = 1
         self.speed_x = 0
         self.speed_y = 0
+        self.w_vision = 800
+        self.h_vision = 600
+
+    def find_vec(self,vec):
+        first = None
+        for num,sign in enumerate(vec):
+            if sign == "<":
+                first = num
+            if sign == ">" and first is not None:
+                second = num
+                res = list(map(float,vec[first+1:second].split(",")))
+                return res    
+        return ""       
+
+    def change_speed(self,vec):
+        vecl = self.find_vec(vec)
+        self.speed_x = vecl[0] * self.abs_speed
+        self.speed_y = vecl[1] * self.abs_speed
+
+    def update(self):
+        self.x += self.speed_x
+        self.y += self.speed_y
+
+def vision(id):
+    for i in range(len(list(players))):
+        #сравнение расстояния между бактериями и диагональю экрана + радиус
+        #цикл попарного прохода по словарю 
+        #если истинно формируем данные к добавлению в список(x,y,size,color)
+        #помещаем данные в шаблон строки
+        #нужно сравнить видимость второго игрока первым
+        pass
 
 engine = create_engine("postgresql+psycopg2://postgres:gh538000@localhost/postgres")
 Session = sessionmaker(engine)
@@ -68,6 +103,7 @@ pg.display.set_caption("bacteria")
 clock = pg.time.Clock()
 
 players = {}
+visible_bac = {}
 
 run = True
 while run:
@@ -82,24 +118,34 @@ while run:
         new_player = Player("test",addr)
         s.merge(new_player)
         s.commit()
+        data = newsocket.recv(1024).decode()
+        name,color = data.split(",")
+        s.query(Player).filter(Player.address == f"({addr[0]},{addr[1]})").update({"name":name,"color":color})
+        s.commit()
         q = s.query(Player).filter(Player.address == f"({addr[0]},{addr[1]})").all()
         for u in q:
-            loc_player = LocalPlayer(u.id,u.name,newsocket,u.address)
+            loc_player = LocalPlayer(u.id,u.name,newsocket,u.address,color)
             players[u.id] = loc_player
     except BlockingIOError:
         pass
 
-    screen.fill("black")
+    for id in list(players):
+        try:
+            data = players[id].socket.recv(1024).decode()
+            players[id].change_speed(data)
+        except:
+            pass
 
     for id in list(players):
-        
+        players[id].update()
+
+    for id in list(players):
+        visible_bac[id] = []
+
+    screen.fill("black")
+
+    for id in list(players):    
         try:
-            for id in list(players):
-                serv_x = round(players[id].x * PROP)
-                serv_y = round(players[id].y * PROP)
-                serv_r = round(players[id].size * PROP)
-                print(serv_x,serv_y,serv_r)
-                pg.draw.circle(screen,(255,0,0),(serv_x,serv_y),serv_r)
             players[id].socket.send("sync".encode())
         except:
             players[id].socket.close()
@@ -107,6 +153,12 @@ while run:
             s.query(Player).filter(Player.id == id).delete()
             s.commit()
             print("socket закрыт")
+    
+    for id in list(players):
+        serv_x = round(players[id].x * PROP)
+        serv_y = round(players[id].y * PROP)
+        serv_r = round(players[id].size * PROP)
+        pg.draw.circle(screen,players[id].color,(serv_x,serv_y),serv_r)
 
     pg.display.update()
 pg.quit()
